ddcb68a50b31969cc20dcd07484b798e0e6ceafd
krolik-ninja <32400053+krolik-ninja@users.noreply.github.com>
2017-09-30 16:50:20 +0200

    zadanie 2

--- js/app.js
+++ js/app.js
@@ -12,5 +12,35 @@ document.addEventListener('DOMContentLoaded', function() {
     forDropdown.addEventListener('mouseout', function(){
         menu.style.display = 'none';
     });
+    //Zadanie 2
+    var buttons = document.querySelectorAll('.read-more');
+    console.log(buttons);
     
+    console.log(buttons[0].previousElementSibling);
+    
+    buttons[0].addEventListener('click', function() {
+        var textArea = this.previousElementSibling;
+        textArea.style.display = 'block'; 
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+    });
+    
+    buttons[0].addEventListener('click', function() {
+
+
+      if (textArea.style.display === 'none' || textArea.style.display === '') {
+
+        textArea.style.display = 'block';
+        
+        textArea.style.display = 'block';
+        this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+      } else {
+
+        textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+      }
+
+    });
+        
+    
+
     });
